b92799f0a8b4f2bc52dec31af64cc932e80fcd21
sriranjanik23 <39214800+sriranjanik23@users.noreply.github.com>
2026-04-06 08:57:05 +0530

    Create TaskManager.php

---
+++ src/TaskManager.php
@@ -0,0 +1,30 @@
+<?php
+
+class TaskManager {
+
+    private $file = 'tasks.json';
+
+    private function readTasks() {
+        // TODO
+    }
+
+    private function writeTasks($tasks) {
+        // TODO
+    }
+
+    public function addTask($title) {
+        // TODO
+    }
+
+    public function listTasks() {
+        // TODO
+    }
+
+    public function updateTask($id, $status) {
+        // TODO
+    }
+
+    public function deleteTask($id) {
+        // TODO
+    }
+}
